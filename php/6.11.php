3fbab75d3db972ee76e4c67cc5b60a8ddb50a7a7
mohamed-bouma <79575387+mohamed-bouma@users.noreply.github.com>
2021-03-15 14:32:25 +0100

    màj 15/03

--- php/6.11.php
+++ php/6.11.php
@@ -1,24 +1,32 @@
 <?php
 
-
-function tab()
+function tab1()
 {
-    $arraysize1 = readline("Entrez le nombre de valeurs du tableau 1 : ");
-    for ($i = 0; $i < $arraysize1; $i++) {
+    $array_size1 = readline("Entrez le nombre de valeurs du tableau 1: ");
+    for ($i = 0; $i < $array_size1; $i++) {
         $tab1[$i] = readline("entrez la valeur numero " . ($i + 1) . " : ");
     }
-    $arraysize2 = readline("Entrez le nombre de valeurs du tableau 2: ");
-    for ($i = 0; $i < $arraysize2; $i++) {
+    return $tab1;
+}
+
+function tab2()
+{
+    $array_size2 = readline("Entrez le nombre de valeurs du tableau 2: ");
+    for ($i = 0; $i < $array_size2; $i++) {
         $tab2[$i] = readline("entrez la valeur numero " . ($i + 1) . " : ");
     }
+    return $tab2;
+}
 
+function schtroumpf($tab1, $tab2)
+{
     $schtroumpf = 0;
-    for ($i = 0; $i < $arraysize1; $i++) {
-        for ($j = 0; $j < $arraysize2; $j++) {
+    for ($i = 0; $i < 4; $i++) {
+        for ($j = 0; $j < 2; $j++) {
             $schtroumpf = $schtroumpf + $tab1[$i] * $tab2[$j];
         }
     }
     echo $schtroumpf;
 }
 
-tab();
+schtroumpf(tab1(), tab2());
